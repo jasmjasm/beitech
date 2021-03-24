b3aa690304648b8dc4d0d79986b330b8403a3c2f
Andres Saldarriaga <jasmjasm@gmail.com>
2021-03-24 14:01:46 -0500

    Customer filter

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from app.models import Customer
 
 # Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        context = super(IndexView, self).get_context_data()
+        context.update({"customers": Customer.objects.all().order_by('name')})
+        return context
